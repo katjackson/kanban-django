0a988933c4c76526b42c674d2e0de2cd43753de8
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-05-18 11:36:36 -0400

    changed index url

--- kanban/board/urls.py
+++ kanban/board/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^/$', views.index_view, name='index')
     url(r'^board/$', views.index_view, name="board"),
     url(r'^register/', views.register, name='register'),
 ]
